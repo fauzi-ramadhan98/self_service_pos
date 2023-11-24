44242a811cecb64929b43020216892183a319bd1
Gamma Teknologi Solusindo <admin@gtssolution.com>
2023-11-25 06:24:51 +0700

    Pertemuan Ke Dua

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/about', function () {
+    $data = [
+        'pageTitle' => 'Tentang Kami',
+        'content' => 'Ini adalah halaman tentang kami.'
+    ];
+    return view('about', $data);
+});
+
+Route::resource('/users', App\Http\Controllers\UsersController);
